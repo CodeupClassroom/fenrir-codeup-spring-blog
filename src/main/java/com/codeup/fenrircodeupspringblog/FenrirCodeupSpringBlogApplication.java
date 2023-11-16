203c4bf324ca4eb0aed9dfecd539ff87438f529b
kennethhowell <howell.kenneth.a@gmail.com>
2023-11-16 14:03:45 -0600

    pushing rest of project template to github - kh

---
+++ src/main/java/com/codeup/fenrircodeupspringblog/FenrirCodeupSpringBlogApplication.java
@@ -0,0 +1,13 @@
+package com.codeup.fenrircodeupspringblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FenrirCodeupSpringBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FenrirCodeupSpringBlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codeup/fenrircodeupspringblog/FenrirCodeupSpringBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeup.fenrircodeupspringblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FenrirCodeupSpringBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
