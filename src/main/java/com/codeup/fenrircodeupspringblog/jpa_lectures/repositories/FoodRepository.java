ea577d5443232315bffd53cc8967e845298b575f
kennethhowell <howell.kenneth.a@gmail.com>
2023-11-20 12:30:05 -0600

    code to point - jpa lecture no relationships

---
+++ src/main/java/com/codeup/fenrircodeupspringblog/jpa_lectures/repositories/FoodRepository.java
@@ -0,0 +1,7 @@
+package com.codeup.fenrircodeupspringblog.jpa_lectures.repositories;
+
+import com.codeup.fenrircodeupspringblog.jpa_lectures.models.Food;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FoodRepository extends JpaRepository<Food, Long> {
+}
