8b5648ada89ecc54933c19857e55d47c10e0e51f
kennethhowell <howell.kenneth.a@gmail.com>
2023-11-20 13:56:54 -0600

    finished jpa-lecture

--- src/main/java/com/codeup/fenrircodeupspringblog/jpa_lectures/repositories/FoodRepository.java
+++ src/main/java/com/codeup/fenrircodeupspringblog/jpa_lectures/repositories/FoodRepository.java
@@ -4,4 +4,11 @@ import com.codeup.fenrircodeupspringblog.jpa_lectures.models.Food;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FoodRepository extends JpaRepository<Food, Long> {
+
+    Food findByNameIs(String name); //SELECT * from food where name = ?;
+
+    Food findByCaloriesIsLessThan(int num); //SELECT * from food where calories < ?;
+
+
+
 }
