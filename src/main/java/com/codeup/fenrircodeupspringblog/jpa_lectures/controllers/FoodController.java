506ea5c3bd244b7abc0aae9e45ecad9532997f69
Gonzalo Ovalle <gonzalo.ovalle01@gmail.com>
2023-11-27 10:07:58 -0600

    completed form model binding lecture

--- src/main/java/com/codeup/fenrircodeupspringblog/jpa_lectures/controllers/FoodController.java
+++ src/main/java/com/codeup/fenrircodeupspringblog/jpa_lectures/controllers/FoodController.java
@@ -1,9 +1,12 @@
 package com.codeup.fenrircodeupspringblog.jpa_lectures.controllers;
 
+import com.codeup.fenrircodeupspringblog.jpa_lectures.models.Food;
 import com.codeup.fenrircodeupspringblog.jpa_lectures.repositories.FoodRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class FoodController {
@@ -25,4 +28,17 @@ public class FoodController {
 
         return "lunch";
     }
+
+    @GetMapping("/lunchOrder")
+    public String lunchOrderForm(Model model) {
+        model.addAttribute("lunchItem", new Food());
+        return "lunchOrderForm";
+    }
+
+    @PostMapping("/lunchOrder")
+    public String createLunchItem(@ModelAttribute Food food) {
+        foodDataAccessObject.save(food);
+        return "redirect:/lunchTime";
+    }
+
 }
